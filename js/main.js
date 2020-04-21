e7d2217c8b7eb53e2889d60a0a6a79a22005da59
kelvin2dan <kelvinyokou@yeah.net>
2020-04-21 16:23:41 +0800

    part of home page

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+$("[name='copyLink']").click(function(sender){
+    copyToClipBoard(sender.target.innerHTML);
+});
+// copy text to clipboard
+function copyToClipBoard(str) {
+    document.copyToClipBoard = str;
+    $('#emailModal').modal('hide');
+    $('#weChatModal').modal('hide');
+    $('#qqModal').modal('hide');
+}
